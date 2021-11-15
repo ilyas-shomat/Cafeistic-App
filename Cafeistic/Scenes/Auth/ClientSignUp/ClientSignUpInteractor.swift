cc05e9fcf12e2cc2b2f26994c1c5c22b52f03ab9
Shomat Ilyas <ilyas.shomat@gmail.com>
2021-11-15 21:54:10 +0600

    update

--- Cafeistic/Scenes/Auth/ClientSignUp/ClientSignUpInteractor.swift
+++ Cafeistic/Scenes/Auth/ClientSignUp/ClientSignUpInteractor.swift
@@ -10,10 +10,38 @@ import Foundation
 
 // MARK: - (Presenter -> Interactor)
 class ClientSignUpInteractor: PresenterToInteractorClientSignUpProtocol {
-    
-    func signUp(userSignUpEntity: SignUpEntity) {
-        
-    }
 
     var presenter: InteractorToPresenterClientSignUpProtocol?
+    var networkApiService: Networkable
+
+    init(networkApiService: Networkable) {
+        self.networkApiService = networkApiService
+    }
+    
+    func signUp(signUpEntity: SignUpEntity) {
+        let signUpEntityRequest = SignUpEntityRequest(
+            fullName: signUpEntity.fullName,
+            username: signUpEntity.username,
+            password: signUpEntity.password,
+            email: signUpEntity.email
+        )
+        postWithSignUpData(signUpEntityRequest: signUpEntityRequest)
+    }
+    
+    func postWithSignUpData(signUpEntityRequest: SignUpEntityRequest) {
+        let target = AuthTarget.clientSignUp(signUpEntityRequest: signUpEntityRequest)
+        networkApiService.load(target: target, jsonType: SignUpEntityResponse.self) { (result) in
+            switch result {
+            case .success(let data):
+                if let errorMessage = data?.errorMessage?.first {
+                    consolePrint(errorMessage)
+                }
+                else {
+                    consolePrint(data)
+                }
+            case .failure(let error):
+                consolePrint(error?.message)
+            }
+        }
+    }
 }
